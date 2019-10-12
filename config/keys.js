bb737d2f5ed88121f762eedc69edcc2281ce6bc2
Peprah Gideon <gideonpeprah94@gmail.com>
2019-10-12 22:54:23 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://gidcode:gidcode@cluster0-vv0id.gcp.mongodb.net/devconnect?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://:@cluster0-vv0id.gcp.mongodb.net/?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
