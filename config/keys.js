016baed21b266c206415045b4e0da9a1f2f10d3e
DESKTOP-QV0I0QS\gid <gideonpeprah94@gmail.com>
2019-10-22 16:14:22 +0100

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://:@cluster0-vv0id.gcp.mongodb.net/?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod')
+}else{
+  module.exports = require('./keys_dev')
+}
