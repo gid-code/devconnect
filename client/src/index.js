6473d8887bca364c06d7ce3ec909ed9258470b7e
DESKTOP-QV0I0QS\gid <gideonpeprah94@gmail.com>
2019-10-22 17:00:39 +0100

    Prepared for deploy again again

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/js/all'
+// import '@fortawesome/fontawesome-free/js/all'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
