4b70fe0d7699760437f607bea367eca1f0ce1bf6
DESKTOP-QV0I0QS\gid <gideonpeprah94@gmail.com>
2019-10-31 18:13:45 +0000

    removed redux devtools from production

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyrity &copy; {new Date().getFullYear()} Dev Zone
+        Copyright &copy; {new Date().getFullYear()} Dev Zone
       </footer>
     </div>
   )
--- client/src/store.js
+++ client/src/store.js
@@ -9,13 +9,32 @@ import rootReducer from './reducers'
 const initialState = {}
 const middleware = [thunk]
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if(process.env.NODE_ENV == 'production'){
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware)
+    )
   )
-)
+}else{
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  )
+}
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// )
 
 export default store
